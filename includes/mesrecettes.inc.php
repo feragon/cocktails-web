db613c0e4a5616452fb7cae983bc01c271347b44
Florian Roméo <florian.romeo@feragon.net>
2017-10-12 20:29:00 +0200

    Optimisation récupération des favoris

--- includes/mesrecettes.inc.php
+++ includes/mesrecettes.inc.php
@@ -1,22 +1,29 @@
 <h1>Mes recettes préférées</h1>
 <hr/>
 
-<?php 
-	
-	foreach($Recettes as $key => $recette) {
-		if(array_key_exists($key, $_SESSION['Favoris'])) {
-			$url_photo = normaliserCaracteres($recette["titre"]);
-			$url_photo = ucfirst(strtolower($url_photo));
-			$url_photo = "resources/photos/".strtr($url_photo, ' ', '_').".jpg";
-			?>
-				<a href='<?php echo "?R=Cocktail&K=".$key; ?>'>
-					<div class="item">
-						<img src="<?php echo file_exists($url_photo) ? $url_photo : "resources/photos/none.jpg"; ?>" />
-						<h4><?php echo $recette["titre"]; ?></h4>
-					</div>
-				</a>
-			<?php
-		}
-	}
-	
-?>
+<?php
+    global $Recettes;
+
+    if(!isset($_SESSION['Favoris'])) {
+        $_SESSION['Favoris'] = array();
+    }
+
+	if(empty($_SESSION["Favoris"])) {
+	    echo 'Vous n\'avez pas de favoris...';
+    }
+    else {
+        foreach($_SESSION['Favoris'] as $key => $favori) {
+            $recette = $Recettes[$key];
+            $url_photo = normaliserCaracteres($recette["titre"]);
+            $url_photo = ucfirst(strtolower($url_photo));
+            $url_photo = "resources/photos/" . strtr($url_photo, ' ', '_') . ".jpg";
+            ?>
+            <a href='<?php echo "?R=Cocktail&K=" . $key; ?>'>
+                <div class="item">
+                    <img src="<?php echo file_exists($url_photo) ? $url_photo : "resources/photos/none.jpg"; ?>"/>
+                    <h4><?php echo $recette["titre"]; ?></h4>
+                </div>
+            </a>
+            <?php
+        }
+    }

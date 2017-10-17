2cf9bdc3aa0828a668bac696a1bd670d99267070
Florian ROMEO <florian.romeo@feragon.net>
2017-10-17 09:48:10 +0200

    register

--- ajax/register.php
+++ ajax/register.php
@@ -1,13 +1,5 @@
 <?php
-$error['login'] = '';
-$error['password'] = '';
-$error['name'] = '';
-$error['lastname'] = '';
-$error['birthdate'] = '';
-$error['email'] = '';
-$error['address'] = '';
-$error['phone'] = '';
-$continue = true;
+require __DIR__ . '/../includes/user_functions.php';
 
 /**
  * Initialise une valeur à une chaine vide dans le tableau
@@ -20,21 +12,24 @@ function init($tableau, $clef) {
     }
 }
 
-if(!isset($_POST['login']) || empty($_POST['login'])) {
-    $error['login'] = 'Login non présent.';
-    $continue = false;
-}
-if(!isset($_POST['password']) || empty($_POST['password'])) {
-    $error['password'] = 'Mot de passe non présent.';
-    $continue = false;
-}
+init($_POST, 'login');
+init($_POST, 'password');
+init($_POST, 'name');
+init($_POST, 'lastname');
+init($_POST, 'birthdate');
+init($_POST, 'email');
+init($_POST, 'address');
+init($_POST, 'phone');
 
-if($continue) {
-    init($_POST, 'name');
-    init($_POST, 'lastname');
-    init($_POST, 'birthdate');
-    init($_POST, 'address');
-    init($_POST, 'phone');
-}
-
+$error = register(
+    $_POST['login'],
+    $_POST['password'],
+    $_POST['name'],
+    $_POST['lastname'],
+    $_POST['gender'],
+    $_POST['email'],
+    $_POST['birthdate'],
+    $_POST['address'],
+    $_POST['phone']
+);
 echo json_encode($error);
--- includes/user_functions.php
+++ includes/user_functions.php
@@ -25,15 +25,67 @@ function verifyPassword($login, $password) {
 }
 
 function register($login, $password, $name, $lastname, $gender, $email, $birthdate, $address, $phone) {
-	$db = getDB();
-	$query = $db->query("
+    $error['login'] = '';
+    $error['password'] = '';
+    $error['name'] = '';
+    $error['lastname'] = '';
+    $error['birthdate'] = '';
+    $error['email'] = '';
+    $error['address'] = '';
+    $error['phone'] = '';
+    $continue = true;
+
+    if(empty($login)) {
+        $error['login'] = 'Login non présent.';
+        $continue = false;
+    }
+    if(empty($password)) {
+        $error['password'] = 'Mot de passe non présent.';
+        $continue = false;
+    }
+
+    if(!$continue) {
+        return $error;
+    }
+
+    if(!empty($name) && !preg_match("#^[a-zA-Z]+$#", $name)) {
+        $error['name'] = 'Prenom invalide';
+        $continue = false;
+    }
+
+    if(!empty($lastname) && !preg_match("#^[a-zA-Z]+$#", $lastname)) {
+        $error['lastname'] = 'Nom invalide';
+        $continue = false;
+    }
+
+    if(!empty($birthdate) && !preg_match("#^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$#", $birthdate)) {
+        $error['birthdate'] = 'Date de naissance invalide';
+        $continue = false;
+    }
+
+    if(!empty($email) && !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $error['email'] = 'Email invalide';
+        $continue = false;
+    }
+
+    if(!empty($phone) && !preg_match("#\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})#", $phone)) {
+        $error['phone'] = 'Numero de telephone invalide';
+        $continue = false;
+    }
+
+    if(!$continue) {
+        return $error;
+    }
+
+    $db = getDB();
+	$query = $db->prepare("
 		INSERT INTO users(login, password, name, lastname, gender, email, birthdate, address, phone) 
 		VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)
 	");
 
 	$query->execute(array(
 		$login,
-		$password,
+		password_hash($password, PASSWORD_DEFAULT),
 		$name,
 		$lastname,
 		$gender,
@@ -42,4 +94,6 @@ function register($login, $password, $name, $lastname, $gender, $email, $birthda
 		$address,
 		$phone
 	));
+
+	return $error;
 }

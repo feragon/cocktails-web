48bb7e1ab20d80b9afbe8d79e0bc7aeba17e1455
Florian Roméo <florian.romeo@feragon.net>
2017-12-03 09:26:42 +0100

    Refactor templates

--- templates/cocktail.inc.php
+++ templates/cocktail.inc.php
@@ -6,26 +6,24 @@ if(!isset($_GET['K']) || !array_key_exists($_GET['K'], $Recettes)) {
 
 require(__DIR__ . '/../ajax/favori.php');
 $retirer = array_key_exists($_GET['K'], $_SESSION['Favoris']);
-
+$ingrs = explode("|", $Recettes[$_GET['K']]['ingredients']);
+$cocktail_prec = ($_GET['K'] > 0) ? $_GET['K']-1 : $_GET['K'];
+$cocktail_suiv = ($_GET['K'] < count($Recettes)-1) ? $_GET['K']+1 : $_GET['K'];
 ?>
 <main>
     <div class="cocktail_desc">
         <h1><?php echo $Recettes[$_GET['K']]['titre'] ?></h1>
 
-        <a class="arrow" href="?R=Cocktail&K=<?php echo ($_GET['K'] > 0) ? $_GET['K']-1 : $_GET['K']; ?>"><i class='fa fa-arrow-circle-o-left'></i></a>
-        <a class="arrow" href="?R=Cocktail&K=<?php echo ($_GET['K'] < count($Recettes)-1) ? $_GET['K']+1 : $_GET['K']; ?>"><i class='fa fa-arrow-circle-o-right'></i></a>
+        <a class="arrow" href="?R=Cocktail&K=<?php echo $cocktail_prec ?>"><i class='fa fa-arrow-circle-o-left'></i></a>
+        <a class="arrow" href="?R=Cocktail&K=<?php echo $cocktail_suiv ?>"><i class='fa fa-arrow-circle-o-right'></i></a>
 
         <form id="formFavori" method="POST" onsubmit="event.preventDefault();">
             <?php
             if($retirer) {
-            ?>
-                <input type="hidden" name="remove" value="<?=$_GET['K']?>">
-            <?php
+                echo '<input type="hidden" name="remove" value="', $_GET['K'], '">';
             }
             else {
-            ?>
-                <input type="hidden" name="add" value="<?=$_GET['K']?>">
-            <?php
+				echo '<input type="hidden" name="add" value="', $_GET['K'], '">';
             }
             ?>
             <button id="retirer" class="boutonRondPlein <?php echo $retirer ? '' : 'hidden'; ?>" type="submit" onclick="removeFavori(<?=$_GET['K']; ?>)">
@@ -41,24 +39,21 @@ $retirer = array_key_exists($_GET['K'], $_SESSION['Favoris']);
             </button>
         </form>
 		
-<?php 
+        <?php
 			$url = getImageURL($Recettes[$_GET['K']]["titre"], false);
-			if(!empty($url))
-				echo "
-		<img src='" . $url . "' alt='cocktail_image' />";
-?>
+			if(!empty($url)) {
+				echo "<img src='" . $url . "' alt='cocktail_image' />";
+			}
+        ?>
         <br/>
 		
         <h3>Ingrédients :</h3>
 		<ul>
-<?php
-            $ingrs = explode("|", $Recettes[$_GET['K']]['ingredients']);
-
+        <?php
             foreach($ingrs as $ing) {
-                echo "
-			<li><i class='fa fa-tint'></i><span>".$ing."</span></li>";
+                echo "<li><i class='fa fa-tint'></i><span>".$ing."</span></li>";
             }
-?>
+        ?>
 		</ul>
 		
         <h3>Préparation :</h3>
--- templates/ingredients.inc.php
+++ templates/ingredients.inc.php
@@ -1,60 +1,55 @@
+<?php
+global $Recettes;
+$step = isset($_GET['C']) ? $_GET['C'] : "Aliment";
+$path = array();
+
+while($step != "Aliment") {
+	if(array_key_exists($step, $Hierarchie)) {
+		array_push($path, $step);
+		$step = $Hierarchie[$step]['super-categorie'][0];
+	}
+	else break;
+}
+array_push($path, $step);
+
+$categorie = "Aliment";
+if(isset($_GET['C'])) $categorie = $_GET['C'];
+?>
 <main>
     <h1>Nos ingrédients</h1>
 	<hr/>
 
     <?php
-	
-	$step = isset($_GET['C']) ? $_GET['C'] : "Aliment";
-	$path = array();
-	
-	while($step != "Aliment") {
-		if(array_key_exists($step, $Hierarchie)) {
-			array_push($path, $step);
-			$step = $Hierarchie[$step]['super-categorie'][0];
-		}
-		else break;
-	}
-	array_push($path, $step);
-	
 	for($i = count($path)-1; $i > 0; $i--) {
-		echo "<a class='ingrds_path' href=\"?R=Ingredients&C=".$path[$i]."\">".$path[$i]."</a>
-	<i class='fa fa-arrow-right'></i>
-	";
+		echo '<a class="ingrds_path" href="?R=Ingredients&C=', $path[$i], '">', $path[$i], '</a>',
+	         '<i class="fa fa-arrow-right"></i>';
 	}
-	echo "<a class='ingrds_path ingrds_path_selected' href=\"?R=Ingredients&C=".$path[0]."\">".$path[0]."</a>
-	";
-	
-?>
+
+	echo '<a class="ingrds_path ingrds_path_selected" href="?R=Ingredients&C=', $path[0], '">', $path[0], '</a>';
+    ?>
 
 	<br/><br/>
 
-    <ul class="liste_ingr"><?php
-			
-	$categorie = "Aliment";
-	if(isset($_GET['C'])) $categorie = $_GET['C'];
-	
+    <ul class="liste_ingr">
+    <?php
 	if(array_key_exists($categorie, $Hierarchie)) {
 		if(array_key_exists('sous-categorie', $Hierarchie[$categorie])) {
 		
 			foreach($Hierarchie[$categorie]['sous-categorie'] as $key => $value) {
-				echo "
-		<li><a href=\"?R=Ingredients&C=".str_replace(" ", "%20", $value)."\"><i class='fa fa-tint fa-fw'></i> ".$value."</a></li>";
+			    $url_key = str_replace(" ", "%20", $value);
+				echo '<li><a href="?R=Ingredients&C=', $url_key, '"><i class="fa fa-tint fa-fw"></i>', $value, '</a></li>';
 			}
 			
-			echo "
-			<li class='flex-line-breaker'></li>";
+			echo '<li class="flex-line-breaker"></li>';
 		}
 		
 		foreach($Recettes as $key => $recette) {
 			foreach($recette['index'] as $ingredient) {
 				if($ingredient == $categorie)
-					echo "
-		<li class='li_cocktail'><a href=\"?R=Cocktail&K=".$key."\"><i class='fa fa-glass fa-fw'></i> ".$recette['titre']."</a></li>";
+					echo '<li class="li_cocktail"><a href="?R=Cocktail&K=',$key, '"><i class="fa fa-glass fa-fw"></i>', $recette['titre'], '</a></li>';
 			}
 		}
 	}
-	
-?>
-
+    ?>
     </ul>
 </main>
